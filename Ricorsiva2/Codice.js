96a322dac52d09fd6117a22e8a069fcdd21b69cc
Average_Sun_Praiser <124038852+ZecchinPietro@users.noreply.github.com>
2023-02-28 10:27:09 +0100

    Inizio della funzione
    Obiettivo: stampare in modo concentrico numeri a partire da uno. L'utente mette il numero da raggiungere e la macchina stampa righe e colonne di numeri in modo CONCENTRICO fino a 1.

---
+++ Ricorsiva2/Codice.js
@@ -0,0 +1,8 @@
+Boh
+
+let colonne, righe, n;
+document.getElementById("numero").value;
+function grandezzaQuad(colonne, righe, n){ // calcolo numero colonne e righe
+    colonne = 2n -1;
+    righe = colonne;
+}
