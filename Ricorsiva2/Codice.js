9f5707a8c581bc9d44c619afc3b3ef5995f62cf0
Average_Sun_Praiser <124038852+ZecchinPietro@users.noreply.github.com>
2023-03-04 11:54:18 +0100

    Add files via upload

--- Ricorsiva2/Codice.js
+++ Ricorsiva2/Codice.js
@@ -1,8 +1,28 @@
-Boh
+let quadrato = [];
+function stampaquadrato(n) {
+    for (let i = 0; i < 2*n - 1; i++) {
+      quadrato.push([]);
+      for (let j = 0; j < 2*n - 1; j++) {
+        quadrato[i].push(0);
+      }
+    }
 
-let colonne, righe, n;
-document.getElementById("numero").value;
-function grandezzaQuad(colonne, righe, n){ // calcolo numero colonne e righe
-    colonne = 2n -1;
-    righe = colonne;
-}
+    let considerato = n;
+    for (let i = 0; i < n; i++) {
+      for (let j = i; j < 2*n - i - 1; j++) {
+        quadrato[i][j] = considerato;
+        quadrato[2*n - i - 2][j] = considerato;
+        quadrato[j][i] = considerato;
+        quadrato[j][2*n - i - 2] = considerato;
+      }
+      considerato--;
+    }
+    
+    for (let i = 0; i < 2*n - 1; i++) {
+      console.log(quadrato[i].join(' '));
+      document.write(quadrato[i].join(' '));
+      document.write("<br>");
+    }
+  }
+    let n = prompt("inserisci numero");
+    stampaquadrato(n);
